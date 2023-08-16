1f593953f64c142d4ed1580c7a5bcf63a08cc6b5
Gi-Rodrigues <140345024+Gi-Rodrigues@users.noreply.github.com>
2023-08-15 22:53:36 -0300

    Update exercicio8.c

--- exercicio8.c
+++ exercicio8.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-
-int main() {
-
-    int i;
-	
-    while (i != 1) {
-        printf("Escolha um numero de 1 a 10:");
-        scanf("%d", &i);
-
-        if (i == 1)
-            printf("Parabens, voce acertou!\n");
-        else
-            printf("Você errou, tente novamente.\n");
-    }
-
-    return 0; 
-}
+// .
